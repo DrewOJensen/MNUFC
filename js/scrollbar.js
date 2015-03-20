c4a3b9574a3920edcc8d8472d59068964c88f008
Drew Jensen <jens0771@gmail.com>
2015-03-20 11:09:42 -0500

    Added scrollbar top offset and scrollbar pixel height.

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
       $("html").niceScroll({
-        cursorcolor: '#8DC8E8'
+        cursorcolor: '#8DC8E8',
+        cursorfixedheight: '100',
+        railpadding: {top:52,right:0,left:0,bottom:0},
       });
     });
